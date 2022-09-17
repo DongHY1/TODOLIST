3ae3fe5cc3421c823c41772463b89308a8766681
haoyudong <2070343054@email.szu.edu.cn>
2022-09-17 22:06:48 +0800

    update node_env

--- server/db.js
+++ server/db.js
@@ -1,18 +1,19 @@
 const { Pool } = require('pg');
 let pool;
-if (process.env.NODE_ENV === 'production') {
-  pool = new Pool({
-    user: 'root',
-    password: 'root',
-    host: 'postgrel',
-  });
-} else {
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
   pool = new Pool({
     user: 'masahiro',
     password: 'dhywj123',
     host: 'localhost',
     port: 5432,
     database: 'todolist',
+  });
+} else {
+  pool = new Pool({
+    user: 'root',
+    password: 'root',
+    host: 'postgrel',
   });
 }
 module.exports = pool;
