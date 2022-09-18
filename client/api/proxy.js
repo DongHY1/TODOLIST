0ecb2c2f371f975e0941f28e79d2750d53beabe5
haoyudong <2070343054@email.szu.edu.cn>
2022-09-18 13:53:48 +0800

    update cors

--- client/api/proxy.js
+++ client/api/proxy.js
@@ -1,19 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = (req, res) => {
-  let target = '';
 
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/todos')) {
-    target = 'http://114.132.221.182:5001';
+const apiProxy = createProxyMiddleware({
+  target: "http://114.132.221.182:5001",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/todos": "" // strip "/api" from the URL
   }
-  // 创建代理对象并转发请求
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      '^/todos/': '/'
-    }
-  })(req, res);
+});
+
+// Expose the proxy on the "/api/*" endpoint.
+export default function (req, res) {
+  return apiProxy(req, res);
 };
