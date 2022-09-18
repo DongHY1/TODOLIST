299bd4c744aaea605f80f4124ca73c746f71d8e8
haoyudong <2070343054@email.szu.edu.cn>
2022-09-18 13:58:36 +0800

    update proxy

--- client/api/proxy.js
+++ client/api/proxy.js
@@ -2,10 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const apiProxy = createProxyMiddleware({
   target: "http://114.132.221.182:5001/",
-  changeOrigin: true,
-  pathRewrite: {
-    "^/todos": "" // strip "/api" from the URL
-  }
+  changeOrigin: true
 });
 
 // Expose the proxy on the "/api/*" endpoint.
