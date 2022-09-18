a1507442831d97a981f07ca9bb8167ffb969cde1
haoyudong <2070343054@email.szu.edu.cn>
2022-09-18 11:16:03 +0800

    update docker

--- server/db.js
+++ server/db.js
@@ -1,19 +1,16 @@
 const { Pool } = require('pg');
-let pool;
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
-  pool = new Pool({
-    user: 'masahiro',
-    password: 'dhywj123',
-    host: 'localhost',
-    port: 5432,
-    database: 'todolist',
-  });
-} else {
-  pool = new Pool({
-    user: 'root',
-    password: 'root',
-    host: 'postgrel',
-  });
-}
+// if you use pnpm run dev
+// const pool = new Pool({
+//   user: 'masahiro',
+//   password: 'dhywj123',
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'todolist',
+// });
+const pool = new Pool({
+  user: 'root',
+  password: 'root',
+  host: 'postgres',
+});
 module.exports = pool;
